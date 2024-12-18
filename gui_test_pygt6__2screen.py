4da1592f316d7a01a7ca3b84f815b1dda8206728
utku <utku353@gmail.com>
2024-12-19 00:41:17 +0300

    added map_point_to_projector function for gui_test_pygt6__2screen

--- gui_test_pygt6__2screen.py
+++ gui_test_pygt6__2screen.py
@@ -6,6 +6,38 @@ from PyQt6.QtGui import QImage, QPixmap, QGuiApplication
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
 
 
+def map_point_to_projector(point, corners, projector_resolution):
+    """
+    point: (x, y) coordinates of the point to be mapped
+    corners: List of 4 corners in the photo
+    projector_resolution : (width, height) of the projector.
+    """
+    # Unpack inputs
+    x, y = point
+    (proj_width, proj_height) = projector_resolution
+
+    corner_coords = sorted(corners, key=lambda x: (x[1], x[0]))  # Sort by y first, then x
+    top_left, top_right = sorted(corner_coords[:2], key=lambda x: x[0])
+    bottom_left, bottom_right = sorted(corner_coords[2:], key=lambda x: x[0])
+
+    src_points = np.array([top_left, top_right, bottom_right, bottom_left], dtype=np.float32)
+    dst_points = np.array([[0, 0], [proj_width, 0], [proj_width, proj_height], [0, proj_height]], dtype=np.float32)
+
+    perspective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+
+    # Apply perspective transform to the input point
+    point_homogeneous = np.array([[x, y]], dtype=np.float32).reshape(-1, 1, 2)  # Input point as 1x1x2 array
+    transformed_point = cv2.perspectiveTransform(point_homogeneous, perspective_matrix)
+
+    # Extract the x, y coordinates of the transformed point
+    projected_x, projected_y = transformed_point[0][0]
+
+    # Check if the point lies within the projector's screen boundaries
+    if 0 <= projected_x <= proj_width and 0 <= projected_y <= proj_height:
+        return (int(projected_x), int(projected_y))
+    else:
+        return 0
+
 class AnotherWindow(QMainWindow):
     width_s=0
     height_s=0
@@ -122,7 +154,7 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.button = QPushButton("Push for Window")
+        self.button = QPushButton("Calibrate")
         self.button.clicked.connect(self.show_new_window)
         self.setCentralWidget(self.button)
 
